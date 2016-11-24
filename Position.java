647f9f6e6ecd2d31179e633f86e2f9e9805bd94b
c2huc2hu <c2huc2hu@gmail.com>
2016-11-24 00:39:03 -0500

    hopefully position will be non zero now. Robot.position is now updated only when the tachometer is reset.
    maybe call tacho reset every time we turn

--- Position.java
+++ Position.java
@@ -1,21 +1,26 @@
 
 public class Position {
-	public double x, y; 
+	public double x, y;
 	public Position(double x, double y) {
-		this.x = x; 
-		this.y = y; 
+		this.x = x;
+		this.y = y;
 	}
-	
+
 	public void increment (Position p){
-		this.x += p.x; 
-		this.y += p.y; 
+		this.x += p.x;
+		this.y += p.y;
 	}
-	
-	public void increment(double x, double y) { 
-		
+
+	public void increment(double x, double y) {
+		this.x += x;
+		this.y += y;
 	}
-	
+
+	public static Position add(Position p, Position q) {
+		return new Position(p.x + q.x, p.y + q.y);
+	}
+
 	public String toString() {
-		return "" + this.x + ", " + this.y; 
+		return "" + this.x + ", " + this.y;
 	}
 }
--- Robot.java
+++ Robot.java
@@ -5,18 +5,19 @@ import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
 
 public class Robot {
-	private static float GRIPPER_SPEED = 90f; 
+	private static float GRIPPER_SPEED = 90f;
 	private static float GRIPPER_GEAR_RATIO = 3f;
-	
+
 	private static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S2);
 	private static EV3UltrasonicSensor sonicSensor = new EV3UltrasonicSensor(SensorPort.S3);
-	private static EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S4);  
-	// public static float[] COLOUR_VALUES = { 0.102f, 0.160f, 0.312f, 0.507f, 0.582f }; 
-	public static Position position = new Position(0, 0); 
+	private static EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S4);
+	// public static float[] COLOUR_VALUES = { 0.102f, 0.160f, 0.312f, 0.507f, 0.582f };
+	public static Position position = new Position(0, 0);
+	private static Position lastFixedPosition = new Position(0, 0); // this is updated when we reset tacho
 	public static int ticksSinceLastObstacle = 0;
-	
-	public static float color, sonic, gyro; 
-								
+
+	public static float color, sonic, gyro, dist;
+
 	public static void drive(float l, float r) {
 		// B-> to left C-> to right
 		Motor.B.setSpeed(Math.abs(l));
@@ -37,59 +38,63 @@ public class Robot {
 			Motor.C.stop(true);
 		}
 	}
-	
-	/* Makes the ultrasonic sensor look in a given direction */ 
+
+	/* Makes the ultrasonic sensor look in a given direction */
 	public static void look(int deg) {
 		// always start sensor pointing straight forward. The allowed motion is then [-90,90]
 		scaledDeg = ultrasonicGearRatio*deg;
-		Motor.D.rotate(scaledDeg);	
+		Motor.D.rotate(scaledDeg);
 	}
-	
-	/* 
-	 * Uses the gripper to grab the object. Note: this function is blocking 
-	 * */ 
+
+	/*
+	 * Uses the gripper to grab the object. Note: this function is blocking
+	 * */
 	public static void grab() {
     	float GRIPPER_CLOSED_POSITION = 90; // angle that gripper should be rotated to
     	dist_goal = 1.5;
-    
-    	look(0); 
-    	float dist = pollSonic(true); // from here  
-    	while (dist > dist_goal) {
-    		float dist = pollSonic(true);
-    		Motor.A.forward(); 
-    	} // to here should be removed 
+
+    	look(0);
+    	float _dist = pollSonic(false); // from here
+    	while (_dist > dist_goal) {
+    		float _dist = pollSonic(false);
+    		Motor.A.forward();
+    	} // to here should be removed
     	Motor.A.setSpeed(GRIPPER_SPEED);
     	Motor.A.rotate(GRIPPER_CLOSED_POSITION * GRIPPER_GEAR_RATIO);
 	}
 
 	public static void drop(){
     	float GRIPPER_OPEN_POSITION = -90;
-    
+
     	Motor.A.setSpeed(GRIPPER_SPEED);
     	Motor.A.rotate(GRIPPER_OPEN_POSITION * GRIPPER_GEAR_RATIO);
 	}
-	/* 
+
+	public static void turn() {
+		// to be written
+	}
+	/*
 	public static void rotate(float s, int l, int r) {
 		// B-> to left C-> to right
-		// use s as a base speed for motor B arbitrarily 
+		// use s as a base speed for motor B arbitrarily
 		Motor.B.setSpeed(Math.abs(s));
 		Motor.C.setSpeed(Math.abs(s));
 		Motor.B.rotate(l,true);
 		Motor.C.rotate(r);
-	} */ 
-	
-	/* 
+	} */
+
+	/*
 	public static void arc(float s, int l, int r) {
 		// B-> to left C-> to right
-		// use s as a base speed for motor B arbitrarily 
-		float speedC = s * r / l; 
-		
+		// use s as a base speed for motor B arbitrarily
+		float speedC = s * r / l;
+
 		Motor.B.setSpeed(Math.abs(s));
 		Motor.C.setSpeed(Math.abs(speedC));
 		Motor.B.rotate(l,true);
 		Motor.C.rotate(r);
-	} */ 
-	
+	} */
+
 	public static float pollColor(boolean log) {
 		int sampleSize = colorSensor.sampleSize();
 		float[] redsample = new float[sampleSize];
@@ -100,7 +105,7 @@ public class Robot {
 		}
 		return redsample[0];
 	}
-	
+
 	public static float pollSonic(boolean log) {
 		int sampleSize = sonicSensor.sampleSize();
 		float[] sample = new float[sampleSize];
@@ -114,7 +119,7 @@ public class Robot {
 
 	private static float pollDist(boolean log) {
 		float convS = 360f/16.8f;
-		float _dist = (Motor.C.getTachoCount()+Motor.B.getTachoCount())/2.0f/convS; 
+		float _dist = (Motor.C.getTachoCount()+Motor.B.getTachoCount())/2.0f/convS;
 		if (log) {
 			System.out.print("dist: ");
 			System.out.println(_dist);
@@ -123,25 +128,28 @@ public class Robot {
 	}
 	private static float pollGyro(boolean log) {
 		float[] sample = new float[gyroSensor.sampleSize()];
-		gyroSensor.getAngleMode().fetchSample(sample, 0); 
+		gyroSensor.getAngleMode().fetchSample(sample, 0);
 		if (log) {
 			System.out.println("gyroSensor: " + sample[0]);
 		}
-		return sample[0]; 
+		return Math.toRadians(sample[0]);
 	}
-	private static void tachoReset() { 
-		Motor.B.resetTachoCount(); 
-		Motor.C.resetTachoCount(); 
+	/* Resets the tachometer for the motors. make sure you call this when you turn! */
+	public static void tachoReset() {
+		this.lastFixedPosition.increment(dist);
+		Motor.B.resetTachoCount();
+		Motor.C.resetTachoCount();
 	}
-	
+
 	public static Position updateState() {
-		// save sensor readings 
+		// save sensor readings
 		dist = pollDist(false);
-		gyro = Math.toRadians(pollGyro(false));
-		color = pollColor(false); 
-		
-		Robot.position.increment(dist * Math.cos(angle), dist * Math.sin(angle));
+		gyro = pollGyro(false);
+		color = pollColor(false);
+		sonic = pollSonic(false);
+
+		Robot.position = Robot.lastFixedPosition.add(new Position(dist * Math.cos(gyro), dist * Math.sin(gyro)));
 		Robot.tachoReset();
-		return Robot.position; 
+		return Robot.position;
 	}
 }
