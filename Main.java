917f42ff1caf293057133660d67025b98aeaf45c
ahsueh1996 <ahsueh1996@gmail.com>
2016-11-25 20:54:54 -0500

    Update Main.java
    Commented main. Ready to run each branch's task

--- Main.java
+++ Main.java
@@ -4,15 +4,22 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Behavior[] behaviours = {
+			// insert your behavior instantiation here
+			// we should try running combonations of behaviors too at some point to make
+			// sure integration works
 //		  new Avoider(30), // avoider arg is "too close"  
 //		  new Pathfinder(),
-		  new Delivery(-3,0.312f)
+// 		  new Delivery(50,0.312f)
 		}; 
 
 		System.out.println("STARTING MAIN");
 		Button.waitForAnyPress();
 		Robot.gyroReset();
 		Robot.updateState(); 
+		
+		// Code for initial cmd input
+		
+		// Richard might want to put grab pizza here
 		while (Button.ESCAPE.isUp()) {
 			for(int i=0; i<behaviours.length; i++){
 				if(behaviours[i].checkActive()) {
